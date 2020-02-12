9ea2ba2a93909272a0f0b7b53a9864571a9f253e
Ryan Moore <rpmoore8@neiu.edu>
2020-02-12 10:26:18 -0600

    Add outputs for additional lambdas

--- outputs.tf
+++ outputs.tf
@@ -23,9 +23,29 @@ output "notify_slack_lambda_function_arn" {
   value       = element(concat(aws_lambda_function.notify_slack.*.arn, [""]), 0)
 }
 
+output "approve_request_slack_lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = element(concat(aws_lambda_function.approve_request_slack.*.arn, [""]), 0)
+}
+
+output "approve_response_slack_lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = element(concat(aws_lambda_function.approve_response_slack.*.arn, [""]), 0)
+}
+
 output "notify_slack_lambda_function_name" {
   description = "The name of the Lambda function"
   value       = element(concat(aws_lambda_function.notify_slack.*.function_name, [""]), 0)
+}
+
+output "approve_request_slack_lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = element(concat(aws_lambda_function.approve_request_slack.*.function_name, [""]), 0)
+}
+
+output "approve_response_slack_lambda_function_name" {
+  description = "The name of the Lambda function"
+  value       = element(concat(aws_lambda_function.approve_response_slack.*.function_name, [""]), 0)
 }
 
 output "notify_slack_lambda_function_invoke_arn" {
@@ -33,9 +53,29 @@ output "notify_slack_lambda_function_invoke_arn" {
   value       = element(concat(aws_lambda_function.notify_slack.*.invoke_arn, [""]), 0)
 }
 
+output "approve_request_slack_lambda_function_invoke_arn" {
+  description = "The ARN to be used for invoking Lambda function from API Gateway"
+  value       = element(concat(aws_lambda_function.approve_request_slack.*.invoke_arn, [""]), 0)
+}
+
+output "approve_response_slack_lambda_function_invoke_arn" {
+  description = "The ARN to be used for invoking Lambda function from API Gateway"
+  value       = element(concat(aws_lambda_function.approve_response_slack.*.invoke_arn, [""]), 0)
+}
+
 output "notify_slack_lambda_function_last_modified" {
   description = "The date Lambda function was last modified"
   value       = element(concat(aws_lambda_function.notify_slack.*.last_modified, [""]), 0)
+}
+
+output "approve_request_slack_lambda_function_last_modified" {
+  description = "The date Lambda function was last modified"
+  value       = element(concat(aws_lambda_function.approve_request_slack.*.last_modified, [""]), 0)
+}
+
+output "approve_response_slack_lambda_function_last_modified" {
+  description = "The date Lambda function was last modified"
+  value       = element(concat(aws_lambda_function.approve_response_slack.*.last_modified, [""]), 0)
 }
 
 output "notify_slack_lambda_function_version" {
@@ -43,7 +83,28 @@ output "notify_slack_lambda_function_version" {
   value       = element(concat(aws_lambda_function.notify_slack.*.version, [""]), 0)
 }
 
-output "lambda_cloudwatch_log_group_arn" {
+output "approve_request_slack_lambda_function_version" {
+  description = "Latest published version of your Lambda function"
+  value       = element(concat(aws_lambda_function.approve_request_slack.*.version, [""]), 0)
+}
+
+output "approve_response_slack_lambda_function_version" {
+  description = "Latest published version of your Lambda function"
+  value       = element(concat(aws_lambda_function.approve_response_slack.*.version, [""]), 0)
+}
+
+output "notify_lambda_cloudwatch_log_group_arn" {
   description = "The Amazon Resource Name (ARN) specifying the log group"
-  value       = element(concat(aws_cloudwatch_log_group.lambda.*.arn, [""]), 0)
+  value       = element(concat(aws_cloudwatch_log_group.notify_lambda.*.arn, [""]), 0)
+}
+
+output "approve_request_lambda_cloudwatch_log_group_arn" {
+  description = "The Amazon Resource Name (ARN) specifying the log group"
+  value       = element(concat(aws_cloudwatch_log_group.approve_request_lambda.*.arn, [""]), 0)
+
+}
+
+output "approve_response_lambda_cloudwatch_log_group_arn" {
+  description = "The Amazon Resource Name (ARN) specifying the log group"
+  value       = element(concat(aws_cloudwatch_log_group.approve_response_lambda.*.arn, [""]), 0)
 }
